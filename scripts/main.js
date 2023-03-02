3feeccbcf9b426fdebec41d6edb789d61b4ac5b9
Allie Polubiec <alexandra.polubiec@gmail.com>
2023-03-02 10:36:04 -0800

    load JSON data into main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-// Add your code here
+"use strict";
+
+fetch('./data/sample-data.json')
+    .then(response => response.json())
+    .then(data => {
+        // Add your code here
+        
+    });

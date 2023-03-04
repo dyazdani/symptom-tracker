bb2c9076628eabb33fc163962ea8a05c2d1b1321
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-04 05:26:48 -0800

    Use top level await instead of putting all code in then

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,64 @@
-fetch('./data/sample-data.json')
-  .then((response) => response.json())
-  .then((data) => {
-    // Start with looking at "type" key and creating <div> to put in <form>
-    // if <div> not already created.
-    const { symptoms } = data.user.entries[0];
+const data = await fetch('./data/sample-data.json').then((response) => response.json());
 
-    let physicalDivCreated = false;
-    let mentalDivCreated = false;
+// Start with looking at "type" key and creating <div> to put in <form>
+// if <div> not already created.
+const { symptoms } = data.user.entries[0];
 
-    symptoms.forEach((element) => {
-      if (element.type === 'physical' && !physicalDivCreated) {
-        const physical = document.createElement('div');
-        physical.id = 'physical';
-        const physicalTitle = document.createElement('h3');
-        physicalTitle.innerText = 'Physical';
-        physicalDivCreated = true;
-      }
+let physicalDivCreated = false;
+let mentalDivCreated = false;
 
-      if (element.type === 'mental' && !mentalDivCreated) {
-        const mental = document.createElement('div');
-        mental.id = 'mental';
-        const mentalTitle = document.createElement('h3');
-        mentalTitle.innerText = 'Mental';
-        mentalDivCreated = true;
-      }
-    });
+symptoms.forEach((element) => {
+  if (element.type === 'physical' && !physicalDivCreated) {
+    const physical = document.createElement('div');
+    physical.id = 'physical';
+    const physicalTitle = document.createElement('h3');
+    physicalTitle.innerText = 'Physical';
+    physicalDivCreated = true;
+  }
 
-    // ----- Making <fieldset> for <form> -----
+  if (element.type === 'mental' && !mentalDivCreated) {
+    const mental = document.createElement('div');
+    mental.id = 'mental';
+    const mentalTitle = document.createElement('h3');
+    mentalTitle.innerText = 'Mental';
+    mentalDivCreated = true;
+  }
+});
 
-    // The "None" button
-    const noneLabel = document.createElement('label');
-    noneLabel.setAttribute('for', 'none');
-    const noneInput = document.createElement('input');
-    noneInput.setAttribute('type', 'radio');
-    noneInput.setAttribute('name', 'severity');
-    noneInput.setAttribute('value', 0);
-    noneInput.id = 'none';
+// ----- Making <fieldset> for <form> -----
 
-    // The "Mild" button
-    const mildLabel = document.createElement('label');
-    mildLabel.setAttribute('for', 'mild');
-    const mildInput = document.createElement('input');
-    mildInput.setAttribute('type', 'radio');
-    mildInput.setAttribute('name', 'severity');
-    mildInput.setAttribute('value', 1);
-    mildInput.id = 'mild';
+// The "None" button
+const noneLabel = document.createElement('label');
+noneLabel.setAttribute('for', 'none');
+const noneInput = document.createElement('input');
+noneInput.setAttribute('type', 'radio');
+noneInput.setAttribute('name', 'severity');
+noneInput.setAttribute('value', 0);
+noneInput.id = 'none';
 
-    // The "Moderate" button
-    const moderateLabel = document.createElement('label');
-    moderateLabel.setAttribute('for', 'moderate');
-    const moderateInput = document.createElement('input');
-    moderateInput.setAttribute('type', 'radio');
-    moderateInput.setAttribute('name', 'severity');
-    moderateInput.setAttribute('value', 2);
-    moderateInput.id = 'moderate';
+// The "Mild" button
+const mildLabel = document.createElement('label');
+mildLabel.setAttribute('for', 'mild');
+const mildInput = document.createElement('input');
+mildInput.setAttribute('type', 'radio');
+mildInput.setAttribute('name', 'severity');
+mildInput.setAttribute('value', 1);
+mildInput.id = 'mild';
 
-    // The "Severe" button
-    const severeLabel = document.createElement('label');
-    severeLabel.setAttribute('for', 'severe');
-    const severeInput = document.createElement('input');
-    severeInput.setAttribute('type', 'radio');
-    severeInput.setAttribute('name', 'severity');
-    severeInput.setAttribute('value', 3);
-    severeInput.id = 'severe';
-  });
+// The "Moderate" button
+const moderateLabel = document.createElement('label');
+moderateLabel.setAttribute('for', 'moderate');
+const moderateInput = document.createElement('input');
+moderateInput.setAttribute('type', 'radio');
+moderateInput.setAttribute('name', 'severity');
+moderateInput.setAttribute('value', 2);
+moderateInput.id = 'moderate';
+
+// The "Severe" button
+const severeLabel = document.createElement('label');
+severeLabel.setAttribute('for', 'severe');
+const severeInput = document.createElement('input');
+severeInput.setAttribute('type', 'radio');
+severeInput.setAttribute('name', 'severity');
+severeInput.setAttribute('value', 3);
+severeInput.id = 'severe';

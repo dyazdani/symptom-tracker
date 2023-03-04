5bf5e816dc877646aa829adced5716b45364e438
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-03 20:59:31 -0800

    Adding array of unique symptom types in user data

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,23 @@
 fetch('./data/sample-data.json')
   .then((response) => response.json())
   .then((data) => {
-    // Add your code here
+    // Populate subsections for each unique syptom type.
+
+    // Start with searching data for "type" key and if the value is unique,
+    // create a subsection of <section>.
+
+    // create a variable for the array of symptoms
+    const { symptoms } = data.user.entries[0];
+
+    const symptomTypes = [];
+
+    symptoms.forEach((element) => {
+      if (element.type) {
+        if (!symptomTypes.includes(element.type)) {
+          symptomTypes.push(element.type);
+        }
+      }
+    });
+
+    console.log(symptomTypes);
   });

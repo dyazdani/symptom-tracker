2f67ae86606dbcb624f06bbfe65056c2231260e0
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-04 15:19:43 -0800

    Creating <fieldset> and <legend> elements, and moving severity button creation into symptoms.forEach()

--- scripts/main.js
+++ scripts/main.js
@@ -41,43 +41,56 @@ symptoms.forEach((element) => {
   }
 });
 
-// ----- Making <fieldset> for <form> -----
+// Creating symptom name and severity entry to place in <div>
+symptoms.forEach((element) => {
+  if (element.name) {
+    // Creating <fieldset>
+    const fieldset = document.createElement('fieldset');
+    fieldset.id = element.name;
 
-// The "None" button
-const noneLabel = document.createElement('label');
-noneLabel.setAttribute('for', 'none');
-const noneInput = document.createElement('input');
-noneInput.setAttribute('type', 'radio');
-noneInput.setAttribute('name', 'severity');
-noneInput.setAttribute('value', 0);
-noneInput.id = 'none';
+    // Creating  and adding <legend>
+    const legend = document.createElement('legend');
+    legend.innerText = symptom.name;
+    fieldset.appendChild(legend);
 
-// The "Mild" button
-const mildLabel = document.createElement('label');
-mildLabel.setAttribute('for', 'mild');
-const mildInput = document.createElement('input');
-mildInput.setAttribute('type', 'radio');
-mildInput.setAttribute('name', 'severity');
-mildInput.setAttribute('value', 1);
-mildInput.id = 'mild';
+    // Creating radio buttons
+    // The "None" button
+    const noneLabel = document.createElement('label');
+    noneLabel.setAttribute('for', 'none');
+    const noneInput = document.createElement('input');
+    noneInput.setAttribute('type', 'radio');
+    noneInput.setAttribute('name', 'severity');
+    noneInput.setAttribute('value', 0);
+    noneInput.id = 'none';
 
-// The "Moderate" button
-const moderateLabel = document.createElement('label');
-moderateLabel.setAttribute('for', 'moderate');
-const moderateInput = document.createElement('input');
-moderateInput.setAttribute('type', 'radio');
-moderateInput.setAttribute('name', 'severity');
-moderateInput.setAttribute('value', 2);
-moderateInput.id = 'moderate';
+    // The "Mild" button
+    const mildLabel = document.createElement('label');
+    mildLabel.setAttribute('for', 'mild');
+    const mildInput = document.createElement('input');
+    mildInput.setAttribute('type', 'radio');
+    mildInput.setAttribute('name', 'severity');
+    mildInput.setAttribute('value', 1);
+    mildInput.id = 'mild';
 
-// The "Severe" button
-const severeLabel = document.createElement('label');
-severeLabel.setAttribute('for', 'severe');
-const severeInput = document.createElement('input');
-severeInput.setAttribute('type', 'radio');
-severeInput.setAttribute('name', 'severity');
-severeInput.setAttribute('value', 3);
-severeInput.id = 'severe';
+    // The "Moderate" button
+    const moderateLabel = document.createElement('label');
+    moderateLabel.setAttribute('for', 'moderate');
+    const moderateInput = document.createElement('input');
+    moderateInput.setAttribute('type', 'radio');
+    moderateInput.setAttribute('name', 'severity');
+    moderateInput.setAttribute('value', 2);
+    moderateInput.id = 'moderate';
+
+    // The "Severe" button
+    const severeLabel = document.createElement('label');
+    severeLabel.setAttribute('for', 'severe');
+    const severeInput = document.createElement('input');
+    severeInput.setAttribute('type', 'radio');
+    severeInput.setAttribute('name', 'severity');
+    severeInput.setAttribute('value', 3);
+    severeInput.id = 'severe';
+  }
+});
 
 // Creating submit button for bottom of the form
 const submitBtn = document.createElement('input');

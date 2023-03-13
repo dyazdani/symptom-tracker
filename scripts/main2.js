9bdfcd95acce07edb18a140186ef6e8fa7741721
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-13 11:50:22 -0700

    Created renderDate() and renderEmptyState() funcitons

--- scripts/main2.js
+++ scripts/main2.js
@@ -1,5 +1,6 @@
 // *--------------- Establishing initial state of the app -----------------------*
 
+// Current date
 const date = new Date().toLocaleDateString('en-us', {
   weekday: 'long',
   month: 'long',
@@ -21,9 +22,36 @@ const state = {
       ],
     },
   },
-  isSubmitted: false,
-  isInProgress: false,
-  isModalShowing: false,
-  symptomAdded: false,
-  allSelectedSeveritiesShowing: false,
+  //   isSubmitted: false,
+  //   isInProgress: false,
+  //   isModalShowing: false,
+  //   symptomAdded: false,
+  //   allSelectedSeveritiesShowing: false,
 };
+
+// *---------- Storing section node that is parent to the app --------------*
+const section = document.querySelector('section');
+
+// *----------------------- Rendering functions -----------------*
+function renderDate() {
+  const timeElement = document.getElementById('date');
+  timeElement.innerText = date;
+}
+
+function renderEmptyState() {
+  const noRecordP = document.createElement('p');
+  noRecordP.id = 'no-record';
+  noRecordP.innerText = "You don't have a symptom record yet for today.";
+
+  const createRecordBtn = document.createElement('button');
+  createRecordBtn.id = 'create-record';
+  createRecordBtn.setAttribute('type', 'button');
+  createRecordBtn.innerText = 'Create a New Record';
+
+  section.appendChild(noRecordP);
+  section.appendChild(createRecordBtn);
+}
+
+// *----------------------- Initialize app -----------------*
+renderDate();
+renderEmptyState();
